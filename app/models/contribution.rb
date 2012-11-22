f90a80192d94403c1012d7a0ab689dad2e1f7759
David Rueck <drueck@gmail.com>
2012-11-22 15:54:31 -0800

    Added active record associations between contributor and contributions

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,3 +1,7 @@
 class Contribution < ActiveRecord::Base
+
   attr_accessible :amount, :date, :contributor_id, :reference, :type
+
+  belongs_to :contributor
+
 end
--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,3 +1,7 @@
 class Contributor < ActiveRecord::Base
+
   attr_accessible :address, :city, :first_name, :last_name, :state, :zip
+
+  has_many :contributions
+
 end
