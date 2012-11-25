c09f4bb010c2509d89b1bd763fca20b8b7ce0bcd
David Rueck <drueck@gmail.com>
2012-11-24 19:14:30 -0800

    Added controller methods to deal with users and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+
+private
+
+  def current_user_is_admin
+    current_user && current_user.is_admin
+  end
+  helper_method :current_user_is_admin
+
+  def no_admins
+    (User.count(conditions: "user_type = '#{User.admin_user_type}'") == 0)
+  end
+
+  def not_authenticated
+    if no_admins
+      redirect_to new_user_url
+    else
+      redirect_to login_url
+    end
+  end
+
 end
