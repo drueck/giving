cf5b1cc14e6cf60094670dfe2f4b5ccb08309092
David Rueck <drueck@gmail.com>
2012-11-24 22:40:13 -0800

    Filled out users controller and views for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
 
   before_filter :require_admin_or_no_admins, only: [:new]
 
+  def index
+    @users = User.scoped\
+      .order('username')\
+      .paginate(page: params[:page], per_page: 10)
+  end
+
   def new
     @user = User.new
     if no_admins
@@ -14,7 +20,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to login_url, :notice => "Your account was created. Please login to continue."
+      if current_user
+        redirect_to users_url
+      else
+        redirect_to login_url, :notice => "Your account was created. Please login to continue."
+      end
     else
       if no_admins
         render :setup
@@ -24,12 +34,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to users_url
+    else
+      render :edit
+    end
+  end
+
   def require_admin_or_no_admins
     unless no_admins
       require_login
       unless current_user_is_admin
         redirect_to root_url, notice: 'Only admins are allowed to manage users'
       end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user.id == @user.id
+      flash.now[:notice] << 'Cannot delete current user'
+      render :edit
+    else
+      @user.destroy
+      redirect_to users_url, notice: 'User deleted'
     end
   end
 
