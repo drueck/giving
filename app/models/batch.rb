923b42670071c91af51f7a307387b16b34222653
David Rueck <drueck@gmail.com>
2012-12-15 22:58:07 -0800

    Added a couple of helper methods

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,4 +2,12 @@ class Batch < ActiveRecord::Base
 
   has_many :contributions
 
+  def total_contributions
+    self.contributions.reduce(0) { |sum, c| sum += c.amount }
+  end
+
+  def posted_at_string
+    self.posted_at.strftime("%B #{self.posted_at.day.ordinalize}, %Y %l:%M%P")
+  end
+
 end
