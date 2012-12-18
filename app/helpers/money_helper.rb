50a5a178a1a192d506fc4f411531ad941d4ce56e
David Rueck <drueck@gmail.com>
2012-12-18 14:20:51 -0800

    Fixed MoneyHelper#round_to_cent

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -1,8 +1,9 @@
 module MoneyHelper
 
   def round_to_cent(amount)
-    amount = amount.to_f if !amount.respond_to?(:round)
-    BigDecimal.new(amount.round(2), 8)
+    amount = amount.to_f
+    amount_string = '%.2f' % amount
+    BigDecimal.new(amount_string, 8)
   end
   protected :round_to_cent
 
