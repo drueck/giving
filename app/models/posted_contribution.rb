7e8c0ee3b9582f516c5e7a96027f076edd71ca3d
David Rueck <drueck@gmail.com>
2012-12-17 23:49:02 -0800

    Destroying a PostedContribution just sets it's status to 'Deleted'

--- app/models/posted_contribution.rb
+++ app/models/posted_contribution.rb
@@ -4,8 +4,19 @@ class PostedContribution < Contribution
 
   after_initialize :set_status_to_posted
 
+  def destroy
+    mark_deleted
+    save
+  end
+
   def set_status_to_posted
     self.status = 'Posted'
   end
+  protected :set_status_to_posted
+
+  def mark_deleted
+    self.status = 'Deleted'
+  end
+  protected :mark_deleted
 
 end
