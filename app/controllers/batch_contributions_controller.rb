0731a789ffda0a3bb87531b73621c8a1554b62a5
David Rueck <drueck@gmail.com>
2013-12-26 20:21:28 -0800

    Add BatchContributionsController and related views

---
+++ app/controllers/batch_contributions_controller.rb
@@ -0,0 +1,97 @@
+class BatchContributionsController < ApplicationController
+
+	before_action :require_login
+  before_action :find_batch
+  before_action :find_contribution, only: [:edit, :update, :destroy]
+
+  def index
+    setup_for_index
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def new
+    @contribution = new_decorated_contribution
+  end
+
+  def create
+    @contribution = new_decorated_contribution(contribution_params)
+    respond_to do |format|
+      format.html { create_html }
+      format.js { create_js }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @contribution.update(contribution_params)
+      redirect_to batch_contributions_path(@batch)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @contribution.mark_deleted
+    redirect_to batch_contributions_path(@batch)
+  end
+
+  private
+
+  def find_batch
+    @batch = Batch.find(params[:batch_id]).decorate
+  end
+
+  def find_contribution
+    @contribution = Contribution.find(params[:id]).decorate
+  end
+
+  def setup_for_index
+    @contributions = @batch.contributions
+      .order(id: :desc)
+      .paginate(pagination_params).decorate
+    @contribution = new_decorated_contribution
+  end
+
+  def create_html
+    if @contribution.save
+      redirect_to batch_contributions_path(@batch)
+    else
+      render :new
+    end
+  end
+
+  def create_js
+    if @contribution.save
+      setup_for_index
+      render :create
+    else
+      render :refresh_form
+    end
+  end
+
+  def new_decorated_contribution(attrs={})
+    @batch.contributions.build(attrs).decorate
+  end
+
+  def contribution_params
+    parse_date_param
+    params.require(:contribution).permit(:amount, :date, :contributor_id,
+      :reference, :payment_type)
+  end
+
+  def parse_date_param
+    if params[:contribution][:date]
+      params[:contribution][:date] = Chronic.parse(params[:contribution][:date])
+    end
+  end
+
+  def pagination_params
+    { page: params.fetch(:page) { 1 }, per_page: params.fetch(:per_page) { 8 } }
+  end
+
+end
---
+++ spec/controllers/batch_contributions_controller_spec.rb
@@ -0,0 +1,195 @@
+require 'spec_helper'
+
+describe BatchContributionsController do
+
+  before(:each) do
+    @controller.stub(:require_login) { true }
+    @batch = Batch.create
+  end
+
+  describe "#index" do
+    before(:each) do
+      @contribution = FactoryGirl.create(:contribution, batch_id: @batch.id)
+      get :index, batch_id: @batch.id
+    end
+    it "should assign the specified Batch, decorated, to @batch" do
+      expect(assigns(:batch)).to eq @batch
+      expect(assigns(:batch)).to be_decorated
+    end
+    it "should assign a page of decorated Contributions for that batch to @contributions" do
+      expect(assigns(:contributions)).to include(@contribution)
+    end
+    it "should assign a new Contribution for the inline form to @contribution" do
+      expect(assigns(:contribution)).to be_a_kind_of(Contribution)
+      expect(assigns(:contribution).batch_id).to eq @batch.id
+      expect(assigns(:contribution)).not_to be_persisted
+    end
+    context "with render views on" do
+      render_views
+      it "should render the index template" do
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+
+  describe "#new" do
+    before(:each) do
+      get :new, batch_id: @batch.id
+    end
+    it "should assign a new Contribution for the batch to @contribution" do
+      expect(assigns(:contribution)).to be_a_kind_of(Contribution)
+      expect(assigns(:contribution).batch_id).to eq @batch.id
+      expect(assigns(:contribution)).not_to be_persisted
+    end
+    context "with render views on" do
+      render_views
+      it "should render the new template" do
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "#create" do
+    context "when the required attributes are supplied" do
+      before(:each) do
+        @contributor = FactoryGirl.create(:contributor)
+        @attrs = { contributor_id: @contributor.id, batch_id: @batch.id,
+          amount: 9.99, date: "12/1/2013" }
+      end
+      context "regardless of format requested" do
+        it "should create the contribution" do
+          post :create, batch_id: @batch.id, contribution: @attrs
+          expect(@batch.contributions.count).to eq 1
+          contribution = @batch.contributions.first
+          expect(contribution.batch_id).to eq @batch.id
+          expect(contribution.contributor_id).to eq @contributor.id
+        end
+      end
+      context "when requesting html format" do
+        it "should redirect to the batch contributions index" do
+          post :create, batch_id: @batch.id, contribution: @attrs
+          expect(response).to redirect_to(batch_contributions_path)
+        end
+      end
+      context "when requesting js format" do
+        before(:each) do
+          post :create, batch_id: @batch.id, contribution: @attrs, format: :js
+        end
+        it "should assign the first page of contributions for the batch to @contributions" do
+          expect(assigns(:contributions)).to include(@batch.contributions.first)
+        end
+        it "should assign a new decorated contribution for the batch to @contribution" do
+          expect(assigns(:contribution)).to be_a_kind_of(Contribution)
+          expect(assigns(:contribution).batch_id).to eq @batch.id
+          expect(assigns(:contribution)).not_to be_persisted
+        end
+        context "with render views on" do
+          render_views
+          it "should render the create js template" do
+            expect(response).to render_template(:create)
+          end
+        end
+      end
+    end
+    context "when any required attribute is missing" do
+      context "regardless of format requested" do
+        before(:each) do
+          post :create, batch_id: @batch.id, contribution: { invalid_param: true }
+        end
+        it "should not create a contribution" do
+          expect(@batch.contributions.count).to eq 0
+        end
+        it "should assign the decorated contribution with errors to @contribution" do
+          expect(assigns(:contribution)).to be_a_kind_of(Contribution)
+          expect(assigns(:contribution)).to be_decorated
+          expect(assigns(:contribution).errors).not_to be_empty
+          expect(assigns(:contribution).batch_id).to eq @batch.id
+        end
+      end
+      context "with render views on" do
+        render_views
+        context "when requesting html format" do
+          it "should render the new template" do
+            post :create, batch_id: @batch.id, contribution: { invalid_param: true }
+            expect(response).to render_template(:new)
+          end
+        end
+        context "when requesting js format" do
+          it "should render the refresh_form template" do
+            post :create, batch_id: @batch.id, contribution: { invalid_param: true }, format: :js
+            expect(response).to render_template(:refresh_form)
+          end
+        end
+      end
+    end
+  end
+
+  describe "#edit" do
+    before(:each) do
+      @contribution = FactoryGirl.create(:contribution, batch_id: @batch.id)
+      get :edit, batch_id: @batch.id, id: @contribution.id
+    end
+    it "should assign the decorated contribution to @contribution" do
+      expect(assigns(:contribution)).to eq @contribution
+      expect(assigns(:contribution)).to be_decorated
+    end
+    context "with render views on" do
+      render_views
+      it "should render the edit template" do
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe "#update" do
+    before(:each) do
+      @contribution = FactoryGirl.create(:contribution, batch_id: @batch.id)
+    end
+    context "when all of the required attributes are present and valid" do
+      before(:each) do
+        @expected_amount = rand(100..200)
+        post :update, batch_id: @batch.id, id: @contribution.id,
+          contribution: { amount: @expected_amount }
+      end
+      it "should update the contribution" do
+        expect(@contribution.reload.amount).to eq Money.new(@expected_amount * 100)
+      end
+      it "should redirect to the batch contributions index" do
+        expect(response).to redirect_to(batch_contributions_path)
+      end
+    end
+    context "when any required attributes are missing or invalid" do
+      before(:each) do
+        post :update, batch_id: @batch.id, id: @contribution.id, contribution: { amount: 0 }
+      end
+      it "should not update the contribution" do
+        expect(@contribution.reload.amount).not_to eq Money.new(0)
+      end
+      it "should assign the decorated contribution with errors to @contribution" do
+        expect(assigns(:contribution)).to eq @contribution
+        expect(assigns(:contribution)).to be_decorated
+        expect(assigns(:contribution).errors).not_to be_empty
+      end
+      context "with render views on" do
+        render_views
+        it "should render the edit template" do
+          expect(response).to render_template(:edit)
+        end
+      end
+    end
+  end
+
+  describe "#destroy" do
+    before(:each) do
+      @contribution = FactoryGirl.create(:contribution, batch_id: @batch.id)
+      delete :destroy, batch_id: @batch.id, id: @contribution.id
+    end
+    it "should mark the contribution as deleted" do
+      expect(Contribution.unscoped.find(@contribution.id).status).to eq "Deleted"
+    end
+    it "should redirect to the batch contributions index" do
+      expect(response).to redirect_to(batch_contributions_path)
+    end
+  end
+
+end
