a9648864c9222fd04049a1e9590b3d52aa552215
David Rueck <drueck@gmail.com>
2014-10-21 19:50:09 -0700

    Apply results of rake rails:update and springify binstubs
    Basically update things for compatibility with Rails 4.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,11 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # ## Mock Framework
