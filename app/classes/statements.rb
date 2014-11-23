26f8582e847189652eaa3d293ef365738b7fcfd1
David Rueck <drueck@gmail.com>
2014-11-22 18:40:18 -0800

    Refactor statements class to not do work in initialize
    Don't do any work until the statements are needed, and then memoize them
    so that the work only needs to be done once. Break out each thing into a
    well-named function.

--- app/classes/statements.rb
+++ app/classes/statements.rb
@@ -3,29 +3,42 @@ class Statements
   include Enumerable
 
   def initialize(year, options={})
-    self.statement_class = options[:statement_class] || Statement
-    self.year = year
-    make_statements
+    @statement_class = options[:statement_class] || Statement
+    @year = year
   end
 
   def each(&blk)
-    @statements.each(&blk)
+    statements.each(&blk)
   end
 
-  protected
+  private
 
-  attr_accessor :year, :statement_class
+  attr_reader :year, :statement_class, :statements
 
-  def make_statements
-    @statements = []
-    start_date = '1/1/' + year.to_s
-    end_date = '12/31/' + year.to_s
-    contributors = Contributor.joins(:contributions)
-      .where('contributions.date >= :start_date and contributions.date <= :end_date and contributions.status != :deleted',
-        start_date: start_date, end_date: end_date, deleted: 'Deleted').order('name').uniq
-    contributors.each do |contributor|
-      @statements << statement_class.new(contributor, start_date, end_date)
-    end
+  def statements
+    @statements ||=
+      contributors.map { |contributor| statement_for(contributor) }
+  end
+
+  def contributors
+    Contributor.joins(:contributions)
+      .where('contributions.date >= :start_date and '\
+             'contributions.date <= :end_date and '\
+             'contributions.status != :deleted',
+             start_date: start_date, end_date: end_date,
+             deleted: 'Deleted').order('name').uniq
+  end
+
+  def statement_for(contributor)
+    statement_class.new(contributor, start_date, end_date)
+  end
+
+  def start_date
+    '1/1/' + year.to_s
+  end
+
+  def end_date
+    '12/31/' + year.to_s
   end
 
 end
