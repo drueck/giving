267ce0ae6bbcee3aecca3dec48b40399e2e71139
David Rueck <drueck@gmail.com>
2015-01-13 18:34:49 -0800

    Add option of zip file of individual statements
    Now when a user wants to generate statements they can choose to generate
    one large PDF that includes all contributor statements (ideal for
    printing) or to generate a zip file with individual PDFS inside for each
    contributor.
    
    Also added error handling for the case where there are no contributions
    for the year that the user selected. In that case it will just render a
    flash warning instead of generating an empty file.

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,22 +1,78 @@
+require 'zip'
+
 class StatementsController < ApplicationController
 
   before_action :require_login
 
   def new
-    @years = (Contribution.year_range).to_a.reverse
+    load_years
   end
 
   def create
-    year = params[:year] || Time.now.year
-    message = params[:message] || ""
+    if contributions_exist_for?(year)
+      respond_with_statements
+    else
+      render_new_with_warning
+    end
+  end
+
+  private
+
+  def load_years
+    @years = (Contribution.year_range).to_a.reverse
+  end
+
+  def contributions_exist_for?(year)
+    in_specified_year = "#{year}-01-01".."#{year}-12-31"
+    Contribution.where(date: in_specified_year).exists?
+  end
+
+  def respond_with_statements
+    if output_type == "many"
+      send_zip_of_individual_statements
+    else
+      send_pdf_of_all_statements
+    end
+  end
+
+  def send_pdf_of_all_statements
     statements = Statements.new(year)
     pdf = StatementsPdf.new(statements, message: message)
     options = {
       filename: "#{year}-statements.pdf",
-      type: "application/pdf",
-      disposition: 'inline'
+      type: "application/pdf"
     }
-    send_data pdf.render, options
+    send_data(pdf.render, options)
+  end
+
+  def send_zip_of_individual_statements
+    statements = Statements.new(year)
+    zipfile = IndividualStatementsZipFile.new(statements, message: message)
+    File.open(zipfile.path, "r") do |f|
+      send_data(f.read.force_encoding("BINARY"),
+                filename: "#{year}-statements.zip",
+                type: "application/zip")
+    end
+  ensure
+    File.unlink(zipfile.path)
+  end
+
+  def render_new_with_warning
+    flash.now[:warning] = "No contributions were found for #{year}"
+    load_years
+    render :new
+  end
+
+  def year
+    @year ||= params.fetch(:year) { Time.now.year }
+  end
+
+  def message
+    @message ||= params.fetch(:message) { "" }
+  end
+
+  def output_type
+    @output_type ||= params.fetch(:output_type) { "one" }
   end
 
 end
--- spec/controllers/statements_controller_spec.rb
+++ spec/controllers/statements_controller_spec.rb
@@ -1,4 +1,68 @@
 require 'spec_helper'
 
 describe StatementsController do
+
+  before(:each) do
+    @controller.stub(:require_login) { true }
+  end
+
+  describe "#new" do
+    it "assigns an array of relevant years to @years and renders new" do
+      create(:contribution, date: "1999-01-01")
+      create(:contribution, date: "2014-01-01")
+      create(:contribution, date: "2015-01-01")
+
+      get :new
+
+      expect(assigns(:years)).to match_array((1999..2015).to_a)
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "#create" do
+    context "when given a year with no contributions" do
+      it "adds a flash warning, assigns years, renders new" do
+        create(:contribution, date: "2014-01-01")
+
+        post :create, year: 2015
+
+        expect(flash[:warning]).not_to be_nil
+        expect(assigns(:years)).to match_array([2014])
+        expect(response).to render_template(:new)
+      end
+    end
+    context "given a year with contributions and requesting one pdf" do
+      it "responds with a pdf with the expected filename" do
+        create(:contribution, date: "2014-01-01")
+        create(:contribution, date: "2014-02-01")
+
+        post :create, year: 2014, output_type: "one"
+        expect(response.headers["Content-Type"]).to eq "application/pdf"
+        expect(response.headers["Content-Disposition"])
+          .to eq 'attachment; filename="2014-statements.pdf"'
+      end
+    end
+    context "given a year with contributions and requesting a zip with many pdfs" do
+      it "responds with a zip file with the expected filename containing statement pdfs" do
+        create(:contribution, date: "2014-01-01")
+        create(:contribution, date: "2014-02-01")
+
+        post :create, year: 2014, output_type: "many"
+        expect(response.headers["Content-Type"]).to eq "application/zip"
+        expect(response.headers["Content-Disposition"])
+          .to eq 'attachment; filename="2014-statements.zip"'
+
+        zipfile = response.body
+        entry_count = 0
+        Zip::InputStream.open(StringIO.new(zipfile)) do |io|
+          while(entry = io.get_next_entry)
+            expect(entry.name).to end_with(".pdf")
+            entry_count += 1
+          end
+        end
+        expect(entry_count).to eq 2
+      end
+    end
+  end
+
 end
